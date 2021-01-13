f29f4651c426e1fe773c8c1c6409bd703b028433
Reid <rlj0713@gmail.com>
2021-01-13 18:28:12 -0500

    commit tree d121cdee13f85a62e0fbb89765c6ca08e2d5a2bb

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime?(number)
+    if is_composite?(number) == true
+        return false
+    elsif number < 2
+        return false
+    else
+        return true
+    end
+end
+
+
+def is_composite?(number)
+    i = number - 1
+    while i > 1
+      return true if number % i == 0
+      i -= 1
+    end
+  end
